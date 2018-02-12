56f771e0a2af333914b399da55f42876be4a78fa
Odd Gunnar Aspaas <odd.gunnar.aspaas@gmail.com>
2018-02-12 10:17:18 +0100

    updated database sample

--- samples/db_sample_dog_owners.sql
+++ samples/db_sample_dog_owners.sql
@@ -11,21 +11,20 @@ CREATE TABLE Hund (
 	EierPnr		INTEGER NOT NULL,
 	CONSTRAINT Hund_PK PRIMARY KEY (Regnr),
 	CONSTRAINT Hund_FK FOREIGN KEY (EierPnr) REFERENCES Person(Pnr)
-											 ON UPDATE CASCADE
-											 ON DELETE NO ACTION,
-											 
+							ON UPDATE CASCADE
+							ON DELETE NO ACTION,
 	CONSTRAINT Alderssjekk CHECK (Faar > 1980));
     
 CREATE TABLE BittAv (
 	Pnr		INTEGER NOT NULL,
-    Regnr 	INTEGER NOT NULL,
-    Antall	INTEGER DEFAULT 0,
-    CONSTRAINT BittAv_PK PRIMARY KEY (Pnr, Regnr),
-    CONSTRAINT BittAv_FK FOREIGN KEY (Pnr) 	REFERENCES Person(Pnr)
-											ON UPDATE CASCADE
-											ON DELETE CASCADE,
+    	Regnr 	INTEGER NOT NULL,
+    	Antall	INTEGER DEFAULT 0,
+    	CONSTRAINT BittAv_PK PRIMARY KEY (Pnr, Regnr),
+    	CONSTRAINT BittAv_FK FOREIGN KEY (Pnr) 		REFERENCES Person(Pnr)
+								ON UPDATE CASCADE
+								ON DELETE CASCADE,
 	CONSTRAINT BittAv_FK2 FOREIGN KEY (Regnr) 	REFERENCES Hund(Regnr)
-												ON UPDATE CASCADE
-                                                ON DELETE CASCADE,
+								ON UPDATE CASCADE
+                                                		ON DELETE CASCADE,
 	CONSTRAINT AntallSjekk CHECK (Antall >= 0));
     
